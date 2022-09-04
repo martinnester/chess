d712778081390c4a31afce25c9ba45a4d0196d1e
Henry Quan <henry@Henrys-MacBook-Pro.local>
2022-09-04 17:18:33 -0400

    formatting

--- src/main.ts
+++ src/main.ts
@@ -79,28 +79,28 @@ abstract class Piece {
         this.color = color
         this.game = game
     }
-    abstract canMoveTo() : Place[];
-    calcMoves(from:Place,increments:Place, repeat: boolean, places: Place[] = [], onlyIfTakes: boolean = false){
+    abstract canMoveTo(): Place[];
+    calcMoves(from: Place, increments: Place, repeat: boolean, places: Place[] = [], onlyIfTakes: boolean = false) {
 
-        let to = {file: from.file+increments.file, rank: from.rank+increments.rank};
-        if(to.file<0 || to.file>7 ||to.rank<0 || to.rank>7){
+        let to = { file: from.file + increments.file, rank: from.rank + increments.rank };
+        if (to.file < 0 || to.file > 7 || to.rank < 0 || to.rank > 7) {
             return places;
         }
         let toPiece = this.game.get(to);
-        if(toPiece===null){
-        
-            if(repeat)this.calcMoves(to,increments, repeat, places);
-            if(onlyIfTakes === false)places.push(to);
-        }else if(toPiece.color===this.color){
+        if (toPiece === null) {
+
+            if (repeat) this.calcMoves(to, increments, repeat, places);
+            if (onlyIfTakes === false) places.push(to);
+        } else if (toPiece.color === this.color) {
             //do nothing
-        } else if(toPiece.color!==this.color){
+        } else if (toPiece.color !== this.color) {
             places.push(to);
         }
         return places;
     }
-    calcManyMoves(incrementsList: Place[], repeat: boolean, places: Place[] = []){
-        for(let increments of incrementsList){
-            this.calcMoves(this.place,increments,repeat, places)
+    calcManyMoves(incrementsList: Place[], repeat: boolean, places: Place[] = []) {
+        for (let increments of incrementsList) {
+            this.calcMoves(this.place, increments, repeat, places)
         }
         return places;
     }
@@ -114,24 +114,24 @@ abstract class Piece {
 class Pawn extends Piece {
     strings = { black: "♟", white: "♙" };
     canMoveTo(): Place[] {
-        if(this.color = "black") {
-            if(this.place.rank === 1) {
-                return [...this.calcMoves(this.place, {file:1, rank:0}, false), ...this.calcMoves(this.place, {file:2 ,rank:0}, false), ...this.calcMoves(this.place, {file:1, rank:1}, false, [], true), ...this.calcMoves(this.place, {file:1, rank:-1}, false, [], true)]
+        if (this.color = "black") {
+            if (this.place.rank === 1) {
+                return [...this.calcMoves(this.place, { file: 1, rank: 0 }, false), ...this.calcMoves(this.place, { file: 2, rank: 0 }, false), ...this.calcMoves(this.place, { file: 1, rank: 1 }, false, [], true), ...this.calcMoves(this.place, { file: 1, rank: -1 }, false, [], true)]
             }
         }
         else {
-            if(this.place.rank === 6) {
-                return [...this.calcMoves(this.place, {file:-1, rank:0}, false), ...this.calcMoves(this.place, {file:-2 ,rank:0}, false), ...this.calcMoves(this.place, {file:-1, rank:1}, false, [], true), ...this.calcMoves(this.place, {file:-1, rank:-1}, false, [], true)]
+            if (this.place.rank === 6) {
+                return [...this.calcMoves(this.place, { file: -1, rank: 0 }, false), ...this.calcMoves(this.place, { file: -2, rank: 0 }, false), ...this.calcMoves(this.place, { file: -1, rank: 1 }, false, [], true), ...this.calcMoves(this.place, { file: -1, rank: -1 }, false, [], true)]
             }
         }
     }
 }
 
-const straights = [{file:1, rank:0},{file:-1, rank:0},{rank:1, file:0},{rank:-1, file:0}];
+const straights = [{ file: 1, rank: 0 }, { file: -1, rank: 0 }, { rank: 1, file: 0 }, { rank: -1, file: 0 }];
 class Rook extends Piece {
     strings = { black: "♜", white: "♖" };
     canMoveTo(): Place[] {
-        return this.calcManyMoves(straights,true);
+        return this.calcManyMoves(straights, true);
     }
 }
 
@@ -154,7 +154,7 @@ class Bishop extends Piece {
 class Queen extends Piece {
     strings = { black: "♚", white: "♔" };
     canMoveTo(): Place[] {
-        return this.calcManyMoves([...diagonals,...straights],true);
+        return this.calcManyMoves([...diagonals, ...straights], true);
     }
 
 }
@@ -162,15 +162,15 @@ class Queen extends Piece {
 class King extends Piece {
     strings = { black: "♛", white: "♕" };
     canMoveTo(): Place[] {
-        return this.calcManyMoves([...diagonals,...straights],false);
+        return this.calcManyMoves([...diagonals, ...straights], false);
     }
 
 }
 
 
 let game = new Game();
-game.display({rank:1, file:6});
-game.display({rank:6, file:6});
+game.display({ rank: 1, file: 6 });
+game.display({ rank: 6, file: 6 });
 
 // game.display({rank:4, file:4});
 // game.display({rank:4, file:3});
