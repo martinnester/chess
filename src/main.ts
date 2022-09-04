18a680d63f745188ca82e9c7c0da32c049fc4876
Henry Quan <henry@Henrys-MacBook-Pro.local>
2022-09-03 23:29:50 -0400

    board array + pawn addition

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,15 @@ class Game {
     board: Piece[][]
     king?: Piece
     constructor(){
-       this.board = [[new Rook("black",this)]] 
+       this.board = 
+       [[new Rook("black", this),new Knight("black", this),new Bishop("black", this),new Queen("black", this),new King("black", this),new Bishop("black", this),new Knight("black", this),new Rook("black", this)],
+       [new Pawn("black", this),new Pawn("black", this),new Pawn("black", this),new Pawn("black", this),new Pawn("black", this),new Pawn("black", this),new Pawn("black", this),new Pawn("black", this)],
+       [null,null,null,null,null,null,null,null],
+       [null,null,null,null,null,null,null,null],
+       [null,null,null,null,null,null,null,null],
+       [null,null,null,null,null,null,null,null],
+       [new Pawn("white", this),new Pawn("white", this),new Pawn("white", this),new Pawn("white", this),new Pawn("white", this),new Pawn("white", this),new Pawn("white", this),new Pawn("white", this)],
+       [new Rook("white", this),new Knight("white", this),new Bishop("white", this),new Queen("white", this),new King("white", this),new Bishop("white", this),new Knight("white", this),new Rook("white", this)],] 
     }
 }
 
@@ -23,6 +31,14 @@ abstract class Piece {
     }
     abstract canMoveTo() : Place[];
 }
+
+
+class Pawn extends Piece {
+    canMoveTo(): Place[] {
+        throw new Error("Method not implemented.");
+    }
+}
+
 
 class Rook extends Piece {
     canMoveTo(): Place[] {
