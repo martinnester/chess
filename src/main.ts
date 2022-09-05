b5bcb4d5e1fa7e8cfdd8613081547d78838ab36a
Martin Nester <martin@Martins-MacBook-Pro.local>
2022-09-05 00:44:07 -0400

    removing moves that would put player into check

--- src/main.ts
+++ src/main.ts
@@ -6,93 +6,38 @@ function myXOR(a: boolean, b: boolean) {
 
 export class Game {
   board: Piece[][];
-  king?: Piece;
+  kings: King[];
   constructor() {
-    this.board = [
-      [
-        new Rook("black", this),
-        new Knight("black", this),
-        new Bishop("black", this),
-        new Queen("black", this),
-        new King("black", this),
-        new Bishop("black", this),
-        new Knight("black", this),
-        new Rook("black", this),
-      ],
-      [
-        new Pawn("black", this),
-        new Pawn("black", this),
-        new Pawn("black", this),
-        new Pawn("black", this),
-        new Pawn("black", this),
-        new Pawn("black", this),
-        null,
-        new Pawn("black", this),
-      ],
-      [
-        new King("white", this),
-        null,
-        null,
-        null,
-        null,
-        new Rook("white", this),
-        null,
-        null,
-      ],
-      [
-        null,
-        null,
-        new Knight("white", this),
-        null,
-        null,
-        new Queen("black", this),
-        null,
-        null,
-      ],
-      [
-        null,
-        null,
-        null,
-        new Bishop("black", this),
-        new Rook("white", this),
-        null,
-        null,
-        null,
-      ],
-      [null, null, null, null, null, null, new Pawn("black", this), null],
-      [
-        new Pawn("white", this),
-        new Pawn("white", this),
-        new Pawn("white", this),
-        new Pawn("white", this),
-        new Pawn("white", this),
-        new Pawn("white", this),
-        new Pawn("white", this),
-        new Pawn("white", this),
-      ],
-      [
-        new Rook("white", this),
-        new Knight("white", this),
-        new Bishop("white", this),
-        new Queen("white", this),
-        new King("white", this),
-        new Bishop("white", this),
-        new Knight("white", this),
-        new Rook("white", this),
-      ],
-    ];
-
+    this.kings = [];
+    this.board = [];
     for (let r = 0; r < 8; r++) {
+      this.board[r] = [];
       for (let f = 0; f < 8; f++) {
-        let toEdit = this.board[r][f];
-        if (toEdit != null) {
-          toEdit.place = new Place(r, f);
-        }
+        this.board[r][f] = null;
       }
     }
   }
+  tempMovePiece(fr: Place, to: Place): () => void {
+    let save = this.get(to);
+    this.set(to, this.get(fr));
+    this.set(fr, null);
+    return () => {
+      this.tempMovePiece(to, fr);
+      this.set(to, save);
+    };
+  }
+  isChecked(color: "black" | "white") {
+    return (
+      this.kings.findIndex((k) => k.color === color && k.isInCheck()) != -1
+    );
+  }
   get(place: Place) {
     return this.board[place.rank][place.file];
+  }
+  set(place: Place, piece: Piece) {
+    if (piece instanceof King) this.kings.push(piece);
+    if (piece) piece.place = place;
+    this.board[place.rank][place.file] = piece;
   }
   displayPiece(rank: number, file: number): string {
     let dark = myXOR(file % 2 == 0, rank % 2 == 0);
@@ -113,7 +58,7 @@ export class Game {
       }
     }
     if (showMovesFor) {
-      let moves = this.get(showMovesFor).canMoveTo();
+      let moves = this.get(showMovesFor).validMoves();
       for (let { file, rank } of moves) {
         strs[rank][file] = chalk.dim(strs[rank][file]);
       }
@@ -143,6 +88,14 @@ abstract class Piece {
     this.game = game;
   }
   abstract canMoveTo(): Place[];
+  validMoves(): Place[] {
+    return this.canMoveTo().filter((to) => {
+      const undo = game.tempMovePiece(this.place, to);
+      let flag = this.game.isChecked(this.color);
+      undo();
+      return !flag;
+    });
+  }
   calcMoves(
     move: {
       from: Place;
@@ -206,24 +159,24 @@ abstract class Piece {
 
 class Pawn extends Piece {
   strings = { black: "♟", white: "♙" };
+  direction = this.color === "black" ? 1 : -1;
   canMoveTo(): Place[] {
-    let direction = this.color === "black" ? 1 : -1;
     let onStart =
       (this.color === "black" && this.place.rank === 1) ||
       (this.color === "white" && this.place.rank === 6);
     return this.calcManyMovesVerbose([
       {
-        increments: { file: -1, rank: direction },
+        increments: { file: -1, rank: this.direction },
         repeat: 0,
         type: "takesOnly",
       },
       {
-        increments: { file: 1, rank: direction },
+        increments: { file: 1, rank: this.direction },
         repeat: 0,
         type: "takesOnly",
       },
       {
-        increments: { file: 0, rank: direction },
+        increments: { file: 0, rank: this.direction },
         repeat: onStart ? 1 : 0,
         type: "noTakes",
       },
@@ -243,23 +196,20 @@ class Rook extends Piece {
     return this.calcManyMoves(straigts);
   }
 }
-
+const lmoves = [
+  { file: 1, rank: 2 },
+  { file: -1, rank: 2 },
+  { file: -1, rank: -2 },
+  { file: 1, rank: -2 },
+  { file: 2, rank: 1 },
+  { file: -2, rank: 1 },
+  { file: -2, rank: -1 },
+  { file: 2, rank: -1 },
+];
 class Knight extends Piece {
   strings = { black: "♞", white: "♘" };
   canMoveTo(): Place[] {
-    return this.calcManyMoves(
-      [
-        { file: 1, rank: 2 },
-        { file: -1, rank: 2 },
-        { file: -1, rank: -2 },
-        { file: 1, rank: -2 },
-        { file: 2, rank: 1 },
-        { file: -2, rank: 1 },
-        { file: -2, rank: -1 },
-        { file: 2, rank: -1 },
-      ],
-      1
-    );
+    return this.calcManyMoves(lmoves, 1);
   }
 }
 const diagonals = [
@@ -287,14 +237,51 @@ class King extends Piece {
   canMoveTo(): Place[] {
     return this.calcManyMoves([...diagonals, ...straigts], 0);
   }
+  isInCheck() {
+    const test = (
+      incrementsList: Place[],
+      repeat: number,
+      predicate: (piece: Piece) => boolean
+    ) => {
+      return (
+        this.calcManyMovesVerbose(
+          incrementsList.map((m) => ({
+            increments: m,
+            repeat,
+            type: "takesOnly",
+          }))
+        ).filter((place) => {
+          const piece = this.game.get(place);
+          return predicate(piece) && this.color != piece.color;
+        }).length > 0
+      );
+    };
+    return (
+      test(lmoves, 0, (p) => p instanceof Knight) ||
+      test(straigts, 8, (p) => p instanceof Queen || p instanceof Rook) ||
+      test(diagonals, 8, (p) => p instanceof Queen || p instanceof Bishop) ||
+      test([...diagonals, ...straigts], 0, (p) => p instanceof King) ||
+      test(diagonals, 0, (p) => {
+        if (p instanceof Pawn) {
+          return p.direction === Math.sign(this.place.rank - p.place.rank);
+        }
+        return false;
+      })
+    );
+  }
 }
 
 let game = new Game();
-game.display({ rank: 4, file: 4 });
-game.display({ rank: 4, file: 3 });
-game.display({ rank: 3, file: 2 });
-game.display({ rank: 3, file: 5 });
-game.display({ rank: 2, file: 0 });
-// game.display({ rank: 2, file: 6 });
-game.display({ rank: 6, file: 6 });
+game.display();
+let king = new Rook("white", game);
+let knight = new Knight("white", game);
+let rook = new Rook("black", game);
+let rook2 = new King("black", game);
+// game.set({ file: 7, rank: 2 }, rook);
+game.set({ file: 1, rank: 7 }, king);
+game.set({ file: 1, rank: 5 }, rook);
+// game.set({ file: 2, rank: 1 }, knight);
+game.set({ file: 1, rank: 2 }, rook2);
+game.display({ file: 1, rank: 5 });
+
 // console.log(game.board[4][4].canMoveTo())
